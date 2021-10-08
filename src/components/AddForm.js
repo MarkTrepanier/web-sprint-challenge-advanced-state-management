1da15c3e6bcef04f5800641d80300c33f0c86bdd
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-10-08 11:53:39 -0400

    MVP

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -11,7 +11,7 @@ const AddForm = (props) => {
     });
 
     //remove when error state is added
-    const errorMessage = props.error;
+    //const errorMessage = props.error; --removed
 
     const handleChange = e => {
         setState({
@@ -23,8 +23,10 @@ const AddForm = (props) => {
     const handleSubmit = e => {
         e.preventDefault();
         if (state.name === "" || state.position === "" || state.nickname === "") {
-            //add in error action
+            props.setError('oh no, something is wrong')
+            return
         }
+        props.addSmurf(state)
     }
 
     return(<section>
@@ -47,7 +49,7 @@ const AddForm = (props) => {
                 <textarea onChange={handleChange} value={state.description} name="description" id="description" />
             </div>
             {
-                errorMessage && <div data-testid="errorAlert" className="alert alert-danger" role="alert">Error: {errorMessage}</div>
+                props.error && <div data-testid="errorAlert" className="alert alert-danger" role="alert">Error: {props.error}</div>
             }
             <button>Submit Smurf</button>
         </form>
